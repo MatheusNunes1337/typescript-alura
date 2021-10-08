095697234e9e4d257d542063501c3eb2ad4faa05
Matheus <matheusyagami147@gmail.com>
2021-10-08 10:02:02 -0300

    realizada a integração do formulário com o controller

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,8 @@
-import { Negociacao } from './models/Negociacao.js'
+import { NegociacaoController } from "./controllers/NegociacaoController.js";
 
-const negociacao = new Negociacao(new Date(), 10, 5000)
-
-
-
-
+const controller = new NegociacaoController()
+const form = document.querySelector('.form')
+form.addEventListener('submit', event => {
+    event.preventDefault()
+    controller.adiciona()
+})
