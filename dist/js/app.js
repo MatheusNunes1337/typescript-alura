00fd0450f73d362cb231a04ce7c1a293cf6ac272
Matheus <matheusyagami147@gmail.com>
2021-10-08 08:28:08 -0300

    criado o model de negociacao e importado em app.js

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,1 +1,6 @@
-alert('oi')
+import { Negociacao } from './models/Negociacao.js'
+
+const negociacao = new Negociacao(new Date(), 10, 1000)
+console.log(negociacao)
+negociacao.quantidade = 50000
+console.log(negociacao)
