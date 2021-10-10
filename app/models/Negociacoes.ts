0bc6c5b4cf7e04d451079a28515cc15cf9edbf42
Matheus <matheusyagami147@gmail.com>
2021-10-10 19:36:41 -0300

    adicionados comentários no arquivo models/Negociacoes.ts mostrando uma outra forma de declarar arrays no typescript

--- app/models/Negociacoes.ts
+++ app/models/Negociacoes.ts
@@ -2,6 +2,7 @@ import { Negociacao } from "./Negociacao.js";
 
 export class Negociacoes {
     private negociacoes: Array<Negociacao> = []
+    //private negociacoes: Negociacao[] = [] é a mesma coisa que a linha 4
 
     adiciona(negociacao: Negociacao) {
         this.negociacoes.push(negociacao)
@@ -10,6 +11,12 @@ export class Negociacoes {
     lista(): ReadonlyArray<Negociacao> {
         return this.negociacoes
     }
+
+    /*
+    lista(): Readonly Negociacao[] { //mesma coisa que nas linhas acima
+        return this.negociacoes
+    }
+    */
 }
 
 const negociacoes = new Negociacoes()
