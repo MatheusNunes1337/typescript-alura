1dcca55bd7bb63d5bb6048e363cc2e1b8a2be82a
Matheus <matheusyagami147@gmail.com>
2021-10-11 11:47:23 -0300

    Data formatada usando o Intl

--- app/views/NegociacoesView.ts
+++ app/views/NegociacoesView.ts
@@ -21,7 +21,7 @@ export class NegociacoesView {
                 ${model.lista().map(negociacao => {
                     return `
                         <tr>
-                            <td>?</td>
+                            <td>${new Intl.DateTimeFormat().format(negociacao.data)}</td>
                             <td>${negociacao.quantidade}</td>
                             <td>${negociacao.valor}</td>
                         </tr>
@@ -34,8 +34,6 @@ export class NegociacoesView {
     }
 
     update(model: Negociacoes) : void {
-        const template = this.template(model)
-        console.log(template)
         this.elemento.innerHTML = this.template(model)
     }
 }
