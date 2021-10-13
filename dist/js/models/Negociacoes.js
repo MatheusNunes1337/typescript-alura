ecbb74dad953cd8b3e8982e8218ae0ac5cd972e6
Matheus <matheusyagami147@gmail.com>
2021-10-13 11:32:53 -0300

    adicionado o campo removeComments no tsconfig.json, para a remoção dos comentáriosdo código quando convertido para .js

--- dist/js/models/Negociacoes.js
+++ dist/js/models/Negociacoes.js
@@ -1,13 +1,7 @@
 export class Negociacoes {
     constructor() {
         this.negociacoes = [];
-        /*
-        lista(): Readonly Negociacao[] { //mesma coisa que nas linhas acima
-            return this.negociacoes
-        }
-        */
     }
-    //private negociacoes: Negociacao[] = [] é a mesma coisa que a linha 4
     adiciona(negociacao) {
         this.negociacoes.push(negociacao);
     }
